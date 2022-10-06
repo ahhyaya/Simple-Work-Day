86a0aa90e485cd1ed6a8d2007a31e3bff7c9af40
doldol123 <toruig59@gmail.com>
2022-10-05 19:59:39 -0700

    adjust save function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
-var saveBtn = $("#save");
-var eventEl = $("#event");
-
-
+var saveBtn = $(".save");
+var eventEl = $(".description");
+var timeP = $("#time")
 
 var currentDay = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").text(currentDay);
 var currentTime = moment().format("H : mm a");
 $("#currentTime").text(currentTime);
 
+var savedEvent = eventEl.text()
+console.log(savedEvent)
+
+localStorage.getItem(time, description);
+
+
+saveBtn.on("click", function () {
+    var description = $(this).siblings(".description").val();
+    var time = $(this).siblings(".time").text().trim();
+    
+    localStorage.setItem(time, description);
+});
+
+//parseInt   .split   moment.(hours)
+
+//if > grey
+//if === red
+//if < green
 
 
 
-saveBtn.on(function(){
-    localStorage.setItem("event", eventEl);
-})
+// if (curTime < schedulerTime) {
+//     !("#row").addClass("row-grey");
+//     !("#event").addClass("row-grey");
+// }
