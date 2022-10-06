f9be4e5e8ebeb9548c959ecedf28fec24f94367c
doldol123 <toruig59@gmail.com>
2022-10-05 21:57:20 -0700

    adjust local storage function

--- js/script.js
+++ js/script.js
@@ -1,29 +1,56 @@
 var saveBtn = $(".save");
-var eventEl = $(".description");
-var timeP = $("#time")
+// var eventEl = $(".description");
+var time = $(".time").text().trim()
+var description = $(".description").val();
 
 var currentDay = moment().format("dddd, MMM Do YYYY");
 $("#currentDay").text(currentDay);
 var currentTime = moment().format("H : mm a");
 $("#currentTime").text(currentTime);
 
+
+onbeforeunload();
 bgColor();
 
-var savedEvent = eventEl.text()
-console.log(savedEvent)
+function onbeforeunload() {
+    time.innerText = localStorage.getItem(time);
+    description.innerText = localStorage.getItem(description);
+}
+
+
+
+
+// var savedEvent = eventEl.text()
+// console.log(savedEvent)
 
 // localStorage.getItem(time, description); 
-
+// console.log(time)
 
 saveBtn.on("click", function () {
+
     var description = $(this).siblings(".description").val();
     var time = $(this).siblings(".time").text().trim();
 
     localStorage.setItem(time, description);
+
 });
 
+// if (localStorage.getItem(description) != null) {
+//     $('.description').val() = localStorage.getItem(description);
+// }
 
-  
+//     $('.description').val().change(function () {
+//     localStorage.setItem(description, $('.description').val());
+// });
+
+// if (localStorage.getItem(time) != null) {
+//     $('.time').text().trim() = localStorage.getItem(time);
+// }
+
+//     $('.time').text().trim().change(function () {
+//     localStorage.setItem(time, $('.time').val());
+// });
+
 
 function bgColor() {
 
@@ -39,23 +66,19 @@ function bgColor() {
     currentTime = moment().format("HH");
     console.log(currentTime)
 
-    if(schedulerTime < currentTime) {
+    if (schedulerTime < currentTime) {
         // $(".row").addClass("row-grey");
         $(".description").addClass("row-grey");
-    } else if (schedulerTime == currentTime){
+    } else if (schedulerTime == currentTime) {
         // $(".row").addClass("row-red");
         $(".description").addClass("row-red");
-    } else if (schedulerTime > currentTime){
-        // $(".row").addClass("row-green");
+    } else if (schedulerTime > currentTime) {
+        $(".row").addClass("row-green");
         $(".description").addClass("row-green");
     }
-       
+
 }
-//parseInt   .split   moment.(hours)
-
-//if > grey
-//if === red
-//if < green
 
 
+// localStorage.clear();
 
