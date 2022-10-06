6fa280cefa6259a2179c98f23304ed564db6de0b
doldol123 <toruig59@gmail.com>
2022-10-05 20:57:30 -0700

    add dynamic backgroud color to change by different time

--- js/script.js
+++ js/script.js
@@ -7,19 +7,50 @@ $("#currentDay").text(currentDay);
 var currentTime = moment().format("H : mm a");
 $("#currentTime").text(currentTime);
 
+bgColor();
+
 var savedEvent = eventEl.text()
 console.log(savedEvent)
 
-localStorage.getItem(time, description);
+// localStorage.getItem(time, description); 
 
 
 saveBtn.on("click", function () {
     var description = $(this).siblings(".description").val();
     var time = $(this).siblings(".time").text().trim();
-    
+
     localStorage.setItem(time, description);
 });
 
+
+  
+
+function bgColor() {
+
+    var schedulerTime = "";
+    $('.time').each(function () {
+        for (var i = 9; i <= 17; i++) {
+            var thisId = $(this).attr('id', i);
+        }
+        schedulerTime = parseInt($(thisId).text(), 10);
+        console.log(schedulerTime);
+    });
+
+    currentTime = moment().format("HH");
+    console.log(currentTime)
+
+    if(schedulerTime < currentTime) {
+        $(".row").addClass("row-grey");
+        $(".description").addClass("row-grey");
+    } else if (schedulerTime == currentTime){
+        $(".row").addClass("row-red");
+        $(".description").addClass("row-red");
+    } else if (schedulerTime > currentTime){
+        $(".row").addClass("row-green");
+        $(".description").addClass("row-green");
+    }
+       
+}
 //parseInt   .split   moment.(hours)
 
 //if > grey
@@ -28,7 +59,3 @@ saveBtn.on("click", function () {
 
 
 
-// if (curTime < schedulerTime) {
-//     !("#row").addClass("row-grey");
-//     !("#event").addClass("row-grey");
-// }
